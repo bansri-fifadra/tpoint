354a38e95be464a1f9d9302f654931578b913f53
bansri-fifadra <87775870+bansri-fifadra@users.noreply.github.com>
2021-08-20 23:03:59 -0400

    Factorial with Recursion

--- JavaTPoint/src/com/bf/basic/Factorial.java
+++ JavaTPoint/src/com/bf/basic/Factorial.java
@@ -2,21 +2,27 @@ package com.bf.basic;
 
 public class Factorial {
 	public static void main(String[] args) {
-		int n=5;
+		int n=4;
 		int fact = factorial(n);
 		System.out.println("Factorial of number " + n + " is: " + fact);
 	}
 
 	private static int factorial(int n) {
-		int fact = 1;
-		while(n > 0)
+/*		int fact = 1;
+  		while(n > 0)
 		{
-			//n = n*factorial(n-1);
 			fact = fact * n;
 			n--;
 		}
-		System.out.println(n);
+*/
+		int fact = n;
+		if(n > 1)
+			fact *= factorial(n-1);
 		return fact;
+
+/*		if(n <= 1) return n;
+		return n * factorial(n-1);
+*/
 	}
 
 }
